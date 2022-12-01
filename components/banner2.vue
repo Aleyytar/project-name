caa32572585f9d991b21506b8ae886d961bc6680
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2022-12-02 01:39:18 +0300

    create banner2

---
+++ components/banner2.vue
@@ -0,0 +1,46 @@
+<template>
+    <section class="banner2">
+        <section class="image">
+            <h1>Yılın En Büyük İndirimi Başladı!</h1>
+            <section class="firsat_main">
+                <section class="fırsat">
+                    <img src="https://kigili.akinoncdn.com/cms/2022/11/29/a3be6c67-e116-4698-b00d-bd7bdffa8467.jpg" alt="">
+                    <p>Triko Fırsatı</p>
+                    <p>2 Triko 499.90₺</p>
+                    <p> <a href="">Alışverişe Başla</a></p>
+                </section>
+                <section class="fırsat">
+                    <img src="https://kigili.akinoncdn.com/cms/2022/11/29/83d90aa9-f6a9-4136-b1e2-28f9d802da0a.jpg" alt="">
+                    <p>Gömlek Fırsatı</p>
+                    <p>2 Triko 499.90₺</p>
+                    <p> <a href="">Alışverişe Başla</a></p>
+                </section>
+            </section>
+     
+    
+            
+        </section>
+    </section>
+    </template>
+        
+        <style scoped>
+    .banner2{
+        width: 100%;
+    }
+    .firsat_main{
+        display: flex;
+        justify-content: center;
+    }
+    h1{
+        text-align: start;
+        padding-left: 10px;
+        font-weight: bold;
+    }
+    .image{
+       text-align: center;
+    }
+    p{
+        text-align: start;
+    }
+        </style>
+        
