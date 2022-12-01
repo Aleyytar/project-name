eaba7b520c20ffdcf299543052ded6fd347b9bd7
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2022-12-02 01:01:46 +0300

    banner add on the top component-1

---
+++ components/banner.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      isVisible: true,
+    }
+  }
+}
+</script>
+<template>
+<section v-show="isVisible">
+    <img src="https://kigili.akinoncdn.com/cms/2022/11/29/2f7416a4-87d4-4a47-b7c4-f80213179336.png" alt="">
+</section>
+</template>
+
+<style scoped>
+img{
+    margin-top: 18px;
+}
+</style>
