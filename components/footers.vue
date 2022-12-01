5e4796aedfffb31ff3ea411e7aac701172514047
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2022-12-02 02:04:34 +0300

    footer

---
+++ components/footers.vue
@@ -0,0 +1,105 @@
+<template>
+<section class="footer_main">
+    <section class="footer_top">
+        <section class="per">
+            <i class='bx bx-fast-forward' ></i> Hızlı Gönderi
+        </section>
+        <section class="per">
+            <i class='bx bx-shape-circle' ></i> Mağazadan Değişim
+        </section>
+        <section class="per">
+            <i class='bx bx-hive' ></i> Mağazada Ücretsiz Tadilat
+        </section>
+        <section class="per">
+            <i class='bx bxs-package' ></i> Mağazadan Ücretsiz Teslimat
+        </section>
+        <section class="per">
+            <i class='bx bxs-door-open' ></i> Kolay İade
+        </section>
+    </section>
+    <section>
+        <section class="footer_middle">
+                <section class="footer">
+                    <h4>Kurumsal</h4>
+                    <ul>
+                        <li>Hakkımızda</li>
+                        <li>Franchising</li>
+                        <li>Kurumsal Satış</li>
+                        <li>İnsan Kaynakları</li>
+                        <li>Özel Dikim</li>
+                        <li>Çeyiz & Home Bayilik</li>
+                    </ul>
+                    <img src="https://avatars.mds.yandex.net/i?id=47ffbbf67415aa93952adb67abf7ba2a7a3ba845-4935356-images-thumbs&n=13&exp=1" alt="">
+                </section>
+                <section class="footer">
+                    <h4>Kurumsal</h4>
+                    <ul>
+                        <li>Hakkımızda</li>
+                        <li>Franchising</li>
+                        <li>Kurumsal Satış</li>
+                        <li>İnsan Kaynakları</li>
+                        <li>Özel Dikim</li>
+                        <li>Çeyiz & Home Bayilik</li>
+                    </ul>
+                    <img src="https://avatars.mds.yandex.net/i?id=47ffbbf67415aa93952adb67abf7ba2a7a3ba845-4935356-images-thumbs&n=13&exp=1" alt="">
+                </section>
+                <section class="footer">
+                    <img class="abdu" src="https://www.upload.ee/image/14662288/dene.png" alt="">
+                </section>
+                <section class="footer">
+                    <h4>Bizi Takip Edin</h4>
+                    <ul>
+                        <li><i class='bx bxl-facebook-circle' ></i> Facebook</li>
+                        <li><i class='bx bxl-instagram-alt' ></i> İnstagram</li>
+                        <li><i class='bx bxl-pinterest-alt' ></i> Pinterest</li>
+                        <li> <i class='bx bxl-youtube' ></i> Youtube</li>
+                        <li> <i class='bx bxl-blogger' ></i> Çeyiz Blog</li>
+                    </ul>
+                </section>
+        </section>
+    </section>
+</section>
+</template>
+
+<style scoped>
+.footer_main{
+    margin-top: 25px;
+}
+.footer_top{
+    width:100%;
+    display: flex;
+    justify-content: center;
+}
+.per{
+    margin-left:55px;
+    font-size: 18px;
+}
+.per i{
+    color:rgba(167, 167, 167, 0.526);
+}
+.footer_middle{
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    margin-top: 25px;
+    border-top: 1px solid gray;
+    padding-top: 20px;
+    justify-content: space-around;
+}
+.footer{
+    widows: 30%;
+}
+.footer ul li{
+    list-style: none;
+}
+.footer img{
+    width:80px;
+    margin-left: 25px;
+}
+.footer h4{
+    font-weight: bold;
+}
+.abdu{
+    width: 254px !important;
+}
+</style>
---
+++ components/giris.vue
@@ -0,0 +1,50 @@
+<template>
+<section class="login_main">
+    <h3>Giriş Yap</h3>
+    <section class="inputs">
+        <input type="text" placeholder="Kullanıcı Adı">
+        <input type="password" placeholder="Şifre">
+        <input class="submite" type="submit" value="Giriş Yap">
+        <p><a href="#" style="float:right; color:black;">Şifremi Unuttum</a></p>
+        <p style="text-align:center;">ya da</p>
+        <input class="facebook" type="submit" value="Facebook">
+        <p style="text-align:center;margin-top:5px;">Üye değilsen</p>
+        <p style="text-align:center;"><a href="#" style="color:black;">UYE OLSUN</a></p>
+    </section>
+</section>
+</template>
+
+<style scoped>
+.login_main{
+    width: 400px;
+    height: 500px;
+    background-color: white;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    margin: 0 auto;
+    padding: 20px;
+    margin-top: 35px;
+}
+.inputs{
+    display:flex;
+    flex-direction: column;
+}
+.inputs input{
+    padding: 6px;
+    margin-top: 20px;
+}
+.submite{
+    background-color: #48a960;
+    border: none;
+    color:white;
+    padding: 14px !important;
+}
+
+.facebook{
+    background-color: #42539d;
+    text-align: center;
+    color:white;
+    border: none;
+    padding: 14px !important;
+}
+
+</style>
