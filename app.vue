6cbea036a04a8fd9b2ed1061eca56096dab56e41
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2022-12-02 00:59:16 +0300

    app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,73 @@
+
+
+
+<script setup>
+import headerBottom from './components/headerbottom.vue'
+import banner from './components/banner.vue'
+import banner2 from './components/banner2.vue'
+import ccategory from './components/category.vue'
+import item from './components/item.vue'
+import bottomBanner from './components/bottomBanner.vue'
+import footers from './components/footers.vue'
+import giris from './components/giris.vue'
+import uyeol from './components/uyeol.vue'
+</script>
+<script>
+import MainHeader from './components/header.vue'
+export default {
+  data() {
+    return {
+      home: true,
+      girise: false,
+      uyeole: false,
+    }
+  },
+  methods: {
+    login(sonuc) {
+      if(sonuc == 1){
+        this.home = false;
+        this.uyeole = false;
+        this.girise = true;
+
+      }else if(sonuc == 2){
+        this.girise = false;
+        this.uyeole = false;
+        this.home = true;
+      }else{
+        this.girise = false;
+        this.home = false;
+        this.uyeole = true;
+      }
+      return null;
+    },
+    },
+    components: {
+      MainHeader
+    },
+}
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+      <MainHeader v-on:login-form="login($event)"/>
+      <headerBottom/>
+      <div v-if="home" class="test">
+        <banner />
+        <banner2 />
+        <ccategory />
+        <item />
+        <bottomBanner />
+      </div>
+      <section v-if="girise">
+        <giris />
+      </section>
+      <section v-if="uyeole">
+        <uyeol />
+      </section>
+      <footers />
+
 </template>
+
+
+<style scoped>
+
+</style>
+
