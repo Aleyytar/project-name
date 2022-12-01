ed4c90203d94daa990d6c7be3d29576f9894cd46
Ali Haydar Turgut <ali-turguut40@hotmail.com>
2022-12-02 01:06:39 +0300

    add style header

--- components/header.vue
+++ components/header.vue
@@ -36,3 +36,86 @@ export default {
   </section>
 </section>
 </template>
+
+<style scoped>
+  .top_main{
+    width: 100%;
+  }
+  .top{
+    display: block;
+    widows: 100%;
+    height: 35px;
+    background: rgb(85, 147, 142);
+    color: white;
+    text-align: center;
+    font-family: 'Poppins';
+    font-weight: bold;
+    line-height: 35px;
+    cursor: pointer;
+  }
+  @media screen and (max-width: 750px) {
+    .top{
+       background-color: #59CE8F !important;
+       cursor: pointer;
+    }
+ }
+  .main_header{
+    width: 100%;
+    height: 90px;
+  }
+.header_top{
+  display: flex;
+  width: 78%;
+  margin: 0 auto;
+  margin-top: 45px;
+  justify-content: space-between;
+}
+.header_top .logo img{
+ height: 60px;
+ cursor: pointer;
+
+}
+.header_top .search{
+  width: 20%;
+  display: flex;
+}
+.header_top .search input{
+  width: 100%;
+  padding: 10px;
+  border:none;
+  height: 35px;
+  border-bottom: 1px solid gray;
+  outline: none;
+}
+.search i{
+  margin-left: -28px;
+  margin-top: 5px;
+  font-size: 30px;
+  
+}
+
+.search input::placeholder{
+  color:black;
+  font-weight: bold;
+  font-family: 'Poppins';
+}
+.right_bar{
+  display: flex;
+}
+.right_bar li{
+  list-style: none;
+  margin-left: 5px;
+  padding: 5px;
+}
+.right_bar li a{
+  text-decoration: none;
+  color: black;
+}
+.line{
+  width: 1px;
+  background: gray;
+  height: 20px;
+  padding: 0px !important;
+  margin-top: 8px;
+}
+</style>
